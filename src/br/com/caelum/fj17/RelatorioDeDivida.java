949e74f1272b5bebc42bcbce3c3e61f75ae5f4e7
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-06-19 17:46:30 -0300

    CApitulo 04 - Atividade 03 - Criando relatorio com NumberFormat

---
+++ src/br/com/caelum/fj17/RelatorioDeDivida.java
@@ -0,0 +1,39 @@
+package br.com.caelum.fj17;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import br.com.caelum.fj17.util.Cnpj;
+
+public class RelatorioDeDivida {
+
+	public void geraRelatorio(Divida divida, NumberFormat format) {
+
+		System.out.println("Credor: " + divida.getCredor());
+		System.out.println("CNPJ: " + divida.getCnpjCredor().getCnpj());
+		System.out.println("Valor a pagar: "
+				+ format.format(divida.valorAPagar()));
+		System.out.println("Valor total: " + format.format(divida.getTotal()));
+
+	}
+
+	public static void main(String[] args) {
+		Divida divida = new Divida();
+		divida.setCnpjCredor(new Cnpj("123456"));
+		divida.setCredor("Uma empresa");
+		divida.setTotal(200);
+
+		// Formato Moeda
+		NumberFormat formatoBr = NumberFormat.getCurrencyInstance(new Locale(
+				"pt", "BR"));
+		NumberFormat formatoUsa = NumberFormat.getCurrencyInstance(new Locale(
+				"en", "US"));
+
+		RelatorioDeDivida relatorio = new RelatorioDeDivida();
+		relatorio.geraRelatorio(divida, formatoBr);
+		System.out.println("===========\n===========");
+		relatorio.geraRelatorio(divida, formatoUsa);
+
+	}
+
+}
